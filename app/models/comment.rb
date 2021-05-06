04a1e72d5f55148a70a86040ebbb87408e20a014
JohnIvanovich <vazbuz@yandex.ru>
2021-05-06 20:36:34 +0200

    search method for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+  def self.search(str)
+    comment = Comment.arel_table
+
+    where(comment[:author].matches("%#{str}%"))
+      .or(
+        where(comment[:address].matches("%#{str}%"))
+      )
+      .or(
+        where(comment[:body].matches("%#{str}%"))
+      )
+  end
 end
