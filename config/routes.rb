46f5d1fe2b072fcc563dc0850721ddb54e236852
JohnIvanovich <vazbuz@yandex.ru>
2021-05-06 10:44:00 +0200

    adds comments page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'comments#index'
+  get 'comments/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
